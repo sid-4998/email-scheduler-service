a4e3ed0fe2466c49a32a43fcab62940bb00a9e65
sid-4998 <siddhantjha34@gmail.com>
2026-01-20 21:01:28 +0530

    feat: Add bulk jobs functionality

--- Backend/src/microservices/email.scheduler.service.ts
+++ Backend/src/microservices/email.scheduler.service.ts
@@ -1,17 +1,21 @@
 import { emailQueue } from "../queues/email.queue";
 import { ScheduledEmail } from '../../generated/prisma/client';
 
-export async function scheduleEmailJob(email: ScheduledEmail) {
-  const delay = new Date(email.scheduledFor).getTime() - Date.now();
+export async function scheduleEmailJobs(
+  emails: ScheduledEmail[]
+) {
+  const jobs = emails.map(email => {
+    const delay = new Date(email.scheduledFor).getTime() - Date.now();
 
-  await emailQueue.add(
-    "send-email",
-    {
-      scheduledEmailId: email.id,
-    },
-    {
-      delay: Math.max(delay, 0),
-      jobId: email.id,
-    }
-  );
+    return {
+      name: "send-email",
+      data: { scheduledEmailId: email.id },
+      opts: {
+        delay: Math.max(delay, 0),
+        jobId: email.id,
+      },
+    };
+  });
+
+  await emailQueue.addBulk(jobs);
 }
